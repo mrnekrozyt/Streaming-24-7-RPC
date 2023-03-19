1e11654a32249935c869e957680d6d938f0526a9
mrnekrozyt <110512818+mrnekrozyt@users.noreply.github.com>
2023-03-19 12:57:39 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+ 
+server.all('/', (req, res) => {
+  res.send(`NOW GO TO ANY UPTIME APP and paste this link`)
+})
+ 
+function keepAlive() {
+  server.listen(3000, () => { console.log("Server is Ready!!" + Date.now()) });
+}
+ 
+module.exports = keepAlive;
