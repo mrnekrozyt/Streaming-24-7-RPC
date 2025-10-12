c4903b40f6ebf0885627eabdf7b044a7403de699
Replit user <>
2025-10-12 16:50:02 +0000

    Initial commit from Replit

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const Discord = require('discord.js-selfbot-v13');
+const Discord = require("discord.js-selfbot-v13");
 const client = new Discord.Client({
   readyStatus: false,
-  checkUpdate: false
+  checkUpdate: false,
 });
 
-const keepAlive = require('./server.js');
+const keepAlive = require("./server.js");
 keepAlive();
 
-function formatTime() { //Credits to himika#0001 and never#0001
+function formatTime() {
+  //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: "America/New_York", //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
-    hour: 'numeric',
-    minute: 'numeric'
+    hour: "numeric",
+    minute: "numeric",
   };
-  return new Intl.DateTimeFormat('en-US', options).format(date);
+  return new Intl.DateTimeFormat("en-US", options).format(date);
 }
 
-client.on('ready', async () => {
+client.on("ready", async () => {
   console.clear();
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence(client)
-    .setApplicationId('1426964594215227456')
-    .setType('STREAMING')
-    .setURL('https://youtu.be/rtaONidfhG4?si=msMXaAJFR1IO5lEX') //Must be a youtube video link 
-    .setState('Those Eyes')
-    .setName('Is this really love?')
+    .setApplicationId("1426964594215227456")
+    .setType("STREAMING")
+    .setURL("https://youtu.be/rtaONidfhG4?si=msMXaAJFR1IO5lEX") //Must be a youtube video link
+    .setState("Captivates Me")
+    .setName("Is this really love?")
     .setDetails(`Captivates Me [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/1057148615405076491/1144509258868736071/lofi-girl-lofi.gif?width=451&height=402') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('I have fallen for you') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1126786252780339304/1140503094791254056/check.gif') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Im obsessed with you') //Text when you hover the Small image
-    .addButton('e-z.bio', 'https://e-z.bio/developer')
-    .addButton('Subscribe to The Goat', 'https://www.youtube.com/@Jenna_YTthegoat');
+    .setAssetsLargeImage(
+      "https://media.discordapp.net/attachments/1258387249041772646/1426967813058330836/image0.png?ex=68ed26d6&is=68ebd556&hm=a961061e6b7da3bee55347bffdfa6e1e6aac3eda2c1291408be46ae7cb6d4faf&=&format=webp&quality=lossless&width=365&height=639",
+    ) //You can put links in tenor or discord and etc.
+    .setAssetsLargeText("I have fallen for you") //Text when you hover the Large image
+    .setAssetsSmallImage(
+      "https://cdn.discordapp.com/attachments/1258387249041772646/1426968065068761220/heart-beating-heart.gif?ex=68ed2712&is=68ebd592&hm=f567bcfd638e8e2c697629ca966a3c363b0ca59e48f94cefbeeab26be957f62c&",
+    ) //You can put links in tenor or discord and etc.
+    .setAssetsSmallText("Im obsessed with you") //Text when you hover the Small image
+    .addButton("e-z.bio", "https://e-z.bio/developer")
+    .addButton(
+      "Subscribe to The Goat",
+      "https://www.youtube.com/@Jenna_YTthegoat",
+    );
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
@@ -44,7 +52,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `Captivates Me [${newTime}]`;
+      const newDetails = `Those Eyes [${newTime}]`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
@@ -52,5 +60,5 @@ client.on('ready', async () => {
   }, 1000); // Update every second
 });
 
-const mySecret = process.env['TOKEN'];
+const mySecret = process.env["TOKEN"];
 client.login(mySecret);
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = require('express');
+const express = require("express");
 const server = express();
- 
-server.all('/', (req, res) => {
-  res.send(`Bot is alive! Server pinged at ${new Date().toLocaleString()}`)
-})
- 
+
+server.all("/", (req, res) => {
+  res.send(`Bot is alive! Server pinged at ${new Date().toLocaleString()}`);
+});
+
 function keepAlive() {
-  server.listen(3000, () => { 
+  server.listen(3000, () => {
     console.log("Server is Ready!! " + Date.now());
-    
-    setInterval(() => {
-      fetch(`http://localhost:3000`)
-        .then(res => console.log(`Keep-alive ping successful: ${res.status}`))
-        .catch(err => console.log('Keep-alive ping failed:', err.message));
-    }, 5 * 60 * 1000); // Ping every 5 minutes
+
+    setInterval(
+      () => {
+        fetch(`http://localhost:3000`)
+          .then((res) =>
+            console.log(`Keep-alive ping successful: ${res.status}`),
+          )
+          .catch((err) => console.log("Keep-alive ping failed:", err.message));
+      },
+      5 * 60 * 1000,
+    ); // Ping every 5 minutes
   });
 }
- 
-module.exports = keepAlive;
 
+module.exports = keepAlive;
