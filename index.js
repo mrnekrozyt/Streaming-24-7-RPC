d4eb076767442b448358f9f5c10f7e32828a3c50
mrnekrozyt <110512818+mrnekrozyt@users.noreply.github.com>
2026-07-05 14:58:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on("ready", async () => {
     .setName("Is this really love?")
     .setDetails(`Captivates Me [${formatTime()}]`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage("img_981750") //You can put links in tenor or discord and etc.
+    .setAssetsLargeImage("https://cdn.discordapp.com/emojis/1523219969276641381.gif") //You can put links in tenor or discord and etc.
     .setAssetsLargeText("I have fallen for you") //Text when you hover the Large image
     .setAssetsSmallImage("small_pic") //You can put links in tenor or discord and etc.
     .setAssetsSmallText("Im obsessed with you") //Text when you hover the Small image
